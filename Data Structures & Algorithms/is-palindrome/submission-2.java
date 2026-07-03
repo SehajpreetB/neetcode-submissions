352889bbd7f8a56a9f4695c8cc04abad245fa504
SehajpreetB <89849192+SehajpreetB@users.noreply.github.com>
2026-07-03 11:54:05 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+                s = s.toLowerCase();
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+
+            while (i < j && !isAlphaNumeric(s.charAt(i))) {
+                i++;
+            }
+
+            while (i < j && !isAlphaNumeric(s.charAt(j))) {
+                j--;
+            }
+
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= '0' && c <= '9');
+    }
+
+}
